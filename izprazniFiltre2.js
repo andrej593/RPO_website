7028d29e26c3b1cfdb37889c6790c0b36843dc70
evagubensek <44508857+evagubensek@users.noreply.github.com>
2018-12-06 19:09:34 +0100

    Add files via upload

---
+++ izprazniFiltre2.js
@@ -0,0 +1,7 @@
+function brisiFiltre(){
+	var prazno1 = document.getElementById("1--").selected;
+	if(prazno1 == false){
+		document.getElementById("1--").selected = true;
+		
+	}
+}
---
+++ izprazniFiltre3.js
@@ -0,0 +1,6 @@
+function brisiFiltre(){
+	var prazno1 = document.getElementById("1--").selected;
+	if(prazno1 == false){
+		document.getElementById("1--").selected = true;
+	}
+}
