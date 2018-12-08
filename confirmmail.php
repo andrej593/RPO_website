1e22f5ab5f9686b535895bc21bfba95644c69089
TRavnak <tilen.ravnak@studnent.um.si>
2018-12-08 23:32:43 +0100

    Update confirmmail.php

--- confirmmail.php
+++ confirmmail.php
@@ -3,16 +3,26 @@ require "config.php";
 
 $username = $_GET["username"];
 $code=$_GET["code"];
-$query1 = "SELECT *FROM users WHERE username = '$username'";
-$query = mysqli_query($povezava, $query1);
-if(mysqli_num_rows($query)){
-	while($row=mysqli_fetch_assoc($query)){
-		$db_code = $row["confirm_num"];
+$sql = "SELECT *FROM users WHERE username = '".$username."';";
+$result = $povezava->query($sql);
+if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		$db_code = $row["confirmNum"];
 	}
 }
-if($code==$db_code){
-	mysqli_query($povezava, "UPDATE users SET confirm=1 WHERE username='$username'");
-	mysqli_query($povezava, "UPDATE users SET confirm_num=0 WHERE username='$username'");
+if($code===$db_code){
+	$sql="UPDATE users SET confirm=1 WHERE username='".$username."';";
+	if ($povezava->query($sql) === TRUE) {
+		//echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $povezava->error;
+	}
+	$sql="UPDATE users SET confirmNum=0 WHERE username='".$username."';";
+	if ($povezava->query($sql) === TRUE) {
+		//echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $povezava->error;
+	}
 	echo "Vas email je bil potrjen.";
 }
 ?>
